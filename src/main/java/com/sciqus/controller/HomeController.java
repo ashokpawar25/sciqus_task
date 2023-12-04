0f5ac7905a9a10d72885d86d9d6c679b7ab764b7
ashokpawar25 <ashokpawar25052001@gmail.com>
2023-12-04 08:13:55 +0530

    last commit

--- src/main/java/com/sciqus/controller/HomeController.java
+++ src/main/java/com/sciqus/controller/HomeController.java
@@ -1,16 +1,11 @@
 package com.sciqus.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.sciqus.dao.StudentRepositery;
 import com.sciqus.entity.Student;
 
@@ -23,18 +18,6 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home() {
 		return "home";
-	}
-
-	@RequestMapping("/add")
-	@ResponseBody
-	public void addStudent() {
-		String name = "ashok";
-		String email = "abc@gmail.com";
-		String username = "sampleuser";
-		String password = "ashok";
-
-		Student student = new Student(name, email, username, password);
-		studentRepositery.save(student);
 	}
 
 	@GetMapping("/studentList")
@@ -51,26 +34,16 @@ public class HomeController {
 	}
 
 	@PostMapping("/register")
-	@ResponseBody
 	public String registerStudent(@RequestParam String name, @RequestParam String email, @RequestParam String username,
 			@RequestParam String password) {
 
-		// Basic validation
 		if (name.trim().isEmpty() || email.trim().isEmpty() || username.trim().isEmpty() || password.trim().isEmpty()) {
 			return "Please fill in all fields.";
 		}
-
-		// Additional validation (e.g., email format, password strength)
-		// Add your validation logic here
-
-		// Create a new student object
+		
 		Student student = new Student(name, email, username, password);
-
-		// Save the student to the repository
 		studentRepositery.save(student);
-
-		// You can return a success message or redirect to another page
-		return "Registration successful!";
+		return "redirect:/login";
 	}
 
 	@GetMapping("/login")
@@ -80,11 +53,10 @@ public class HomeController {
 
 	@PostMapping("/login")
 	public String login(@RequestParam String username, @RequestParam String password, Model model) {
-		// Your authentication logic here
 		Student student = studentRepositery.findByUsernameAndPassword(username, password);
 
 		if (student != null) {
-			return "redirect:/studentList"; // Redirect to the student list page on successful login
+			return "redirect:/studentList";
 		} else {
 			model.addAttribute("error", "Invalid username or password");
 			return "login";
